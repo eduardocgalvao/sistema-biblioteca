acc47a6c8582cc143cfd2368470ad7faa64dea4d
Murilo Chagas <murilodasilvadaschagas12@gmail.com>
2025-11-28 01:41:23 -0300

    feat: adicionando no painel do /admin todas as tabelas criadas do sistema.

--- config/biblioteca/admin.py
+++ config/biblioteca/admin.py
@@ -1,4 +1,16 @@
 from django.contrib import admin
-from django.urls import include, path
+from . import models
 
-# Register your models here.
+
+# Registra todos os modelos do app `biblioteca` no admin.
+# Você pode customizar a exibição (list_display, search_fields, etc.) conforme necessário.
+admin.site.register(models.tbl_editora)
+admin.site.register(models.tbl_autor)
+admin.site.register(models.tbl_categoria)
+admin.site.register(models.tbl_status_livro)
+admin.site.register(models.tbl_livro)
+admin.site.register(models.tbl_livro_autor)
+admin.site.register(models.tbl_livro_categoria)
+admin.site.register(models.tbl_usuario)
+admin.site.register(models.tbl_motivo_remocao)
+admin.site.register(models.tbl_livro_remocao)
