00eae1f806223797afce1c380cd3c4b4852e794d
Murilo Chagas <murilodasilvadaschagas12@gmail.com>
2025-11-28 01:59:53 -0300

    feat: configurando a rota de login.

--- config/config/urls.py
+++ config/config/urls.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from biblioteca.views import login_view
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('biblioteca/', login_view),
 ]
